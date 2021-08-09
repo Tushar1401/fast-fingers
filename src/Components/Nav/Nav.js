7f062d7242f7f6e3f96e4aab165bc4c0c02d0952
Tushar1401 <tushararora1401@gmail.com>
2021-08-09 17:53:03 +0530

    final changes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
         <div className="nav-container">
             <div className="nav-left">
                 <img src={logo} alt="" className="logo" />
-                <a href="/"><p className="logo-text">Fast Fingers</p></a>
+                <a href="/fast-fingers/"><p className="logo-text">Fast Fingers</p></a>
             </div>
             <div className="nav-right">
                 <a
